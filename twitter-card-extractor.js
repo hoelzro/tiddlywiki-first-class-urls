7b8b7922f3efbf30ea669e09f4f01c5ced5075ca
Rob Hoelz <rob@hoelz.ro>
2020-08-18 22:27:02 -0500

    Add trappings of a Twitter card data extractor

---
+++ twitter-card-extractor.js
@@ -0,0 +1,19 @@
+/*\
+title: $:/plugins/hoelzro/first-class-urls/twitter-card-extractor.js
+type: application/javascript
+module-type: $:/plugin/hoelzro/url-metadata-extractor
+
+\*/
+
+(function() {
+    let { selectAll } = require('css-select');
+    let { getText } = require('domutils');
+
+    exports.extract = function(url, dom) {
+        let metaTags = selectAll('meta[name^="twitter:"]', dom);
+        for(let meta of metaTags) {
+            console.log(meta.attribs.name, meta.attribs.content);
+        }
+        return {};
+    };
+})();
