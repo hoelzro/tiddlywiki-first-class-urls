a0c1d5cb03212dd3dda0cbd10ca9360a39f5ae57
Rob Hoelz <rob@hoelz.ro>
2020-08-23 13:58:34 -0500

    Display the title of the URL tiddler when rendering links
    ...rather than the raw http://… or https://…

---
+++ link-parse-rule.js
@@ -0,0 +1,85 @@
+/*\
+title: $:/plugins/hoelzro/first-class-urls/link-parse-rule.js
+type: application/javascript
+module-type: wikirule
+
+\*/
+
+(function(){
+
+exports.name = 'firstclasslink';
+exports.types = {inline: true};
+
+exports.init = function(parser) {
+    this.parser = parser;
+    // Regexp to match
+    this.matchRegExp = /~?(?:file|http|https|mailto|ftp|irc|news|data|skype):[^\s<>{}\[\]`|"\\^]+(?:\/|\b)/mg;
+};
+
+// XXX duplicated functionality
+function canonicalizeURL(url) {
+    return url;
+}
+
+exports.parse = function() {
+    // Move past the match
+    this.parser.pos = this.matchRegExp.lastIndex;
+    // Create the link unless it is suppressed
+    if(this.match[0].substr(0,1) === '~') {
+        return [{type: 'text', text: this.match[0].substr(1)}];
+    } else {
+        let location = canonicalizeURL(this.match[0]);
+        let fauxWidget = {
+            getVariable(variableName, options) {
+                if(variableName == 'location') {
+                    return location;
+                }
+                if(options && 'defaultValue' in options) {
+                    return options.defaultValue;
+                } else {
+                    return null;
+                }
+            }
+        };
+
+        let linkTextWidget = {
+            type: 'set',
+            attributes: {
+                name: {type: 'string', value: 'location'},
+                value: {type: 'string', value: location}
+            },
+            children: [{
+                type: 'list',
+                isBlock: false, // XXX are you sure?
+                attributes: {
+                    filter: {
+                        type: 'string',
+                        // XXX duplicated functionality from import-handler.js
+                        value: '[field:location<location>has[url_tiddler]!has[draft.of]] ~[<location>]'
+                    },
+                    template: {
+                        type: 'string',
+                        value: '$:/plugins/hoelzro/first-class-urls/link-template'
+                    }
+                }
+            }]
+        };
+
+        return [{
+            type: 'text',
+            text: '🔗 ', 
+        }, {
+            type: 'element',
+            tag: 'a',
+            attributes: {
+                href: {type: 'string', value: this.match[0]},
+                'class': {type: 'string', value: 'tc-tiddlylink-external'},
+                target: {type: 'string', value: '_blank'},
+                rel: {type: 'string', value: 'noopener noreferrer'}
+            },
+            children: [linkTextWidget]
+        }];
+    }
+};
+
+})();
