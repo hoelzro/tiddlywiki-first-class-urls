ffb4ada041ca9f484de4b51f62eaf46377f7bda8
Rob Hoelz <rob@hoelz.ro>
2020-08-31 21:59:38 -0500

    Test that importing a URL that resolves to an existing title doesn't blow the existing tiddler away

--- cypress/integration/import.js
+++ cypress/integration/import.js
@@ -75,4 +75,45 @@ describe('Import functionality', () => {
 
     cy.get('div.tc-import td input[type=checkbox]').should('not.be.checked');
   });
+
+  it('Should not blow away existing tiddlers with the same name', () => {
+    cy.server();
+    cy.route({
+      method: 'PUT',
+      url: '/recipes/default/tiddlers/**',
+      status: 204,
+      headers: {
+        etag: '"default/' + encodeURIComponent('$:/StoryList') + '/1:"' // XXX dynamically determine this based on URL
+      },
+      response: ''
+    });
+
+    cy.visit('http://localhost:9091');
+
+    cy.window().then(win => {
+      win.$tw.wiki.addTiddler(new win.$tw.Tiddler({
+        title: 'GitHub - PuerkitoBio/goquery: A little like that j-thing, only in Go.',
+        type: 'text/vnd.tiddlywiki',
+        text: 'test tiddler content'
+      }));
+    });
+
+    cyPaste(cy.get('div.tc-site-subtitle'), 'text/plain', 'https://github.com/PuerkitoBio/goquery');
+
+    // XXX helper function to get story list or something
+    cy.window().then(win => win.$tw.wiki.getTiddler('$:/StoryList').fields.list).should('include', '$:/Import');
+
+    // XXX wait for server to respond?
+
+    // XXX helper function to get element within tiddler
+    cy.get('div.tc-tiddler-frame[data-tiddler-title="$:/Import"]').get('button').contains('Import').click();
+
+    cy.get('div.tc-tiddler-frame[data-tiddler-title="$:/Import"]').get('a.tc-tiddlylink').contains('goquery').click();
+
+    cy.get('div.tc-search input[type="search"]').type('PuerkitoBio');
+
+    cy.get('div.tc-search-results a.tc-tiddlylink').contains('PuerkitoBio').click();
+
+    cy.get('div.tc-tiddler-frame[data-tiddler-title="GitHub - PuerkitoBio/goquery: A little like that j-thing, only in Go."] div.tc-tiddler-body').contains('test tiddler content');
+  });
 });
