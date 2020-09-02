7cea90234211f1be6f894886640e52db43f94446
Rob Hoelz <rob@hoelz.ro>
2020-09-01 21:05:54 -0500

    Pass location for URL tiddler lookup as variable

--- url-check.js
+++ url-check.js
@@ -3,7 +3,21 @@ module-type: library
 
 \*/
 
+// XXX location is expected to be a canonicalized URL
 module.exports = function(location) {
-    // XXX it would be nice if I could pass location in via a variable or something
-    return $tw.wiki.filterTiddlers('[field:location[' + location + ']has[url_tiddler]]').length > 0;
+    let fauxWidget = {
+        getVariable(variableName, options) {
+            if(variableName == 'location') {
+                return location;
+            }
+            if(options && 'defaultValue' in options) {
+                return options.defaultValue;
+            } else {
+                return null;
+            }
+        }
+    };
+
+    let [tiddler] = $tw.wiki.filterTiddlers('[field:location[' + location + ']has[url_tiddler]!has[draft.of]]', fauxWidget);
+    return tiddler;
 };
