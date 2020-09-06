ac253a38de13d22607732ae57a515d33ee3a9f25
Rob Hoelz <rob@hoelz.ro>
2020-09-06 08:33:06 -0500

    Test that a URL tiddler is created after saving a tiddler with a link

--- cypress/integration/render.js
+++ cypress/integration/render.js
@@ -22,4 +22,27 @@ describe('Render functionality', function() {
         twCypress.getStoryListTiddlerBodyElement('New Tiddler').find('a').contains('Example Domain').should('have.attr', 'href', 'https://example.com');
         twCypress.getStoryListTiddlerBodyElement('New Tiddler').find('a').contains('🔗').should('have.attr', 'href', '#Link%3A%20Example%20Domain');
     });
+
+    it("Creates a URL tiddler after saving a tiddler that includes a link", function() {
+        twCypress.makeServer();
+        twCypress.mockMetadata('https://example.com', {
+            title: 'Example Site',
+            description: 'This is an example'
+        });
+
+        cy.visit('http://localhost:9091');
+
+        // Add a new tiddler that links to that URL
+        twCypress.pageControls.newTiddler().click();
+        twCypress.editor().type('Example link: https://example.com');
+        twCypress.editToolbarControls.save().click();
+
+        twCypress.getTiddler('Link: Example Site').should('have.tw_field', 'description', 'This is an example');
+        twCypress.getTiddler('Link: Example Site').should('have.tw_field', 'url_tiddler', 'true');
+        twCypress.getTiddler('Link: Example Site').should('not.have.tw_field', 'url_tiddler_pending_fetch');
+        twCypress.getTiddler('Link: Example Site').should('have.tw_field', 'location', 'https://example.com');
+
+        twCypress.getStoryListTiddlerBodyElement('New Tiddler').find('a').contains('Example Site').should('have.attr', 'href', 'https://example.com');
+        twCypress.getStoryListTiddlerBodyElement('New Tiddler').find('a').contains('🔗').should('have.attr', 'href', '#Link%3A%20Example%20Site');
+    });
 });
