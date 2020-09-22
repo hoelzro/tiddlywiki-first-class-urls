4f47cd05f2dac7ed5ea90566f127a08ded56dead
Rob Hoelz <rob@hoelz.ro>
2020-09-21 21:21:01 -0500

    Combine base HTML and Twitter card data into fallback extractor
    I don't have a good way in mind to delegate to other extractors, or
    to merge two extractors' results, so let's just have a generic
    "fallback" one

---
+++ fallback-extractor.js
@@ -0,0 +1,35 @@
+/*\
+module-type: $:/plugin/hoelzro/url-metadata-extractor
+
+\*/
+
+(function() {
+    let { selectOne, selectAll } = require('css-select');
+    let { getText } = require('domutils');
+
+    exports.pattern = '**';
+    exports.name = 'fallback';
+
+    exports.extract = function(url, dom) {
+        let titleElement = selectOne('title', dom);
+        let twitterCardMetaTags = selectAll('meta[name^="twitter:"], meta[property^="twitter:"]', dom);
+        let openGraphMetaTags = selectAll('meta[name^="og:"], meta[property^="og:"]', dom);
+
+        let metadata = {
+            title: getText(titleElement),
+        };
+
+        for(let meta of twitterCardMetaTags.concat(openGraphMetaTags)) {
+            let name = meta.attribs.name ?? meta.attribs.property;
+
+            switch(name) {
+                case 'twitter:description':
+                case 'og:description':
+                    metadata.description = meta.attribs.content;
+                    break;
+            }
+        }
+
+        return metadata;
+    };
+})();
