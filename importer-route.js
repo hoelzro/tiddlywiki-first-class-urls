12685827d8d204ac695d0b7821513cfc1083a073
Rob Hoelz <rob@hoelz.ro>
2020-09-21 22:29:58 -0500

    Add route to import links directly into wiki

---
+++ importer-route.js
@@ -0,0 +1,73 @@
+/*\
+module-type: route
+
+GET /plugins/hoelzro/first-class-urls/import?url=:url
+
+\*/
+
+(function() {
+    let { URL } = require('url');
+
+    let {handler: fetchHandler} = require('$:/plugins/hoelzro/first-class-urls/fetcher-route.js');
+    let {onLinksAdded, ALREADY_HAVE_URL, NO_METADATA_TITLE} = require('$:/plugins/hoelzro/first-class-urls/link-added.js');
+
+    exports.method = 'PUT';
+
+    exports.path = new RegExp(`^/plugins/hoelzro/first-class-urls/import`);
+
+    exports.handler = function(request, response, state) {
+        let requestURL = new URL('http://localhost' + request.url);
+        let fetchThisURL = requestURL.searchParams.get('url');
+
+        let fetchStatusCode;
+        let fetchStatus;
+        let fetchStatusHeaders;
+        let fetchBody;
+
+        function fauxFetch({url, callback}) {
+            let fauxResponse = {
+                writeHead(statusCode, status, headers) {
+                    fetchStatusCode = statusCode;
+                    fetchStatus = status;
+                    fetchStatusHeaders = headers;
+                },
+
+                end(body) {
+                    if(fetchStatusCode != 200) {
+                        fetchBody = body;
+                        callback(new Error(fetchStatus));
+                        return;
+                    }
+
+                    callback(null, body);
+                },
+            };
+
+            fetchHandler(request, fauxResponse, state);
+        }
+
+        onLinksAdded($tw.wiki, [fetchThisURL], fauxFetch).then(function([importedTitle]) {
+            if(importedTitle == ALREADY_HAVE_URL) {
+                response.writeHead(409, 'Conflict', {
+                    'Content-Type': 'application/json'
+                });
+                response.end('{}');
+            } else if(importedTitle == NO_METADATA_TITLE) {
+                response.writeHead(400, 'Bad Request', {
+                    'Content-Type': 'application/json'
+                });
+                response.end('{}');
+            } else {
+                response.writeHead(201, 'Created', {
+                    'Content-Type': 'application/json'
+                });
+                response.end(JSON.stringify({
+                    title: importedTitle
+                }));
+            }
+        }, function(error) {
+            response.writeHead(fetchStatusCode, fetchStatus, fetchStatusHeaders);
+            response.end(fetchBody);
+        });
+    };
+})();
