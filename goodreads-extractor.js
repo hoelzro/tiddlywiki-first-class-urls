7e715aa0f545d4658483f3a1dce3902974585eea
Rob Hoelz <rob@hoelz.ro>
2021-05-04 07:19:50 -0500

    Refactor goodreads field extraction
    …into a nice loop over a table of field extraction candidates

--- goodreads-extractor.js
+++ goodreads-extractor.js
@@ -11,27 +11,25 @@ module-type: $:/plugin/hoelzro/url-metadata-extractor
     exports.name = 'goodreads';
 
     exports.extract = function(url, dom) {
-        let openGraphMetaTags = selectAll('meta[name^="og:"], meta[property^="og:"]', dom);
-
         let metadata = {};
 
-        for(let meta of openGraphMetaTags) {
-            switch(meta.attribs.property) {
-                case 'og:title':
-                    metadata.title = meta.attribs.content;
-                    break;
+        let matchers = [
+            ['title', 'meta[property="og:title"]', elems => elems[0].attribs.content],
+            ['goodreads_authors', 'div#bookAuthors a.authorName span[itemprop="name"]', elems => $tw.utils.stringifyList(elems.map(e => getText(e)))],
+            ['description', 'div#descriptionContainer div#description span[id^="freeText"]:not([id^="freeTextContainer"])', elems => getText(elems[0]).trim()],
+            ['description', 'div#descriptionContainer div#description span[id^="freeText"]', elems => getText(elems[0]).trim()],
+        ];
+
+        for(let [metadataField, selector, extractContent] of matchers) {
+            if(metadata.hasOwnProperty(metadataField)) {
+                continue;
+            }
+
+            let matchingElements = selectAll(selector, dom);
+            if(matchingElements.length > 0) {
+                metadata[metadataField] = extractContent(matchingElements);
             }
         }
-
-        let authorElements = selectAll('div#bookAuthors a.authorName span[itemprop="name"]', dom);
-        metadata.goodreads_authors = $tw.utils.stringifyList(authorElements.map(e => getText(e)));
-
-        let descriptionElement = selectOne('div#descriptionContainer div#description span[id^="freeText"]:not([id^="freeTextContainer"])', dom);
-        if(descriptionElement === null) {
-            descriptionElement = selectOne('div#descriptionContainer div#description span[id^="freeText"]', dom);
-        }
-
-        metadata.description = getText(descriptionElement).trim();
 
         return metadata;
     };
