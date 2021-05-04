98bcadb9f7fd0fea20350ed128672851a07cdca0
Rob Hoelz <rob@hoelz.ro>
2021-05-04 06:59:02 -0500

    Add some error catching
    Otherwise there are some situations where an exception thrown from an
    extractor can crash the TiddlyWiki daemon :(

--- fetcher-route.js
+++ fetcher-route.js
@@ -56,36 +56,40 @@ GET /plugins/hoelzro/first-class-urls/fetch?url=:url
             'User-Agent': 'TiddlyWikiFirstClassURLs/1.0.0',
         };
         let req = get(url, {headers}, function(res) {
-            if(res.statusCode >= 200 && res.statusCode < 300) {
-                let chunks = [];
-                res.on('data', (chunk) => chunks.push(chunk));
-                res.on('end', function() {
-                    let responseBody = Buffer.concat(chunks);
-                    if('content-encoding' in res.headers) {
-                        let encoding = res.headers['content-encoding'];
-                        let decompress;
-                        switch(encoding) {
-                            case 'gzip':
-                                decompress = zlib.gunzip;
-                                break;
-                            default:
-                                callback(new Error(`unsupported Content-Encoding: ${encoding}`));
-                                return;
+            try {
+                if(res.statusCode >= 200 && res.statusCode < 300) {
+                    let chunks = [];
+                    res.on('data', (chunk) => chunks.push(chunk));
+                    res.on('end', function() {
+                        let responseBody = Buffer.concat(chunks);
+                        if('content-encoding' in res.headers) {
+                            let encoding = res.headers['content-encoding'];
+                            let decompress;
+                            switch(encoding) {
+                                case 'gzip':
+                                    decompress = zlib.gunzip;
+                                    break;
+                                default:
+                                    callback(new Error(`unsupported Content-Encoding: ${encoding}`));
+                                    return;
+                            }
+                            decompress(responseBody, callback);
+                            return;
                         }
-                        decompress(responseBody, callback);
-                        return;
+                        callback(null, responseBody.toString('utf8'));
+                    });
+                } else if(res.statusCode >= 300 && res.statusCode < 400) {
+                    if(numRedirects > 0) {
+                        let newURL = new URL(res.headers.location, url);
+                        performFetch(newURL, callback, numRedirects - 1);
+                    } else {
+                        callback(new Error('too many redirects'));
                     }
-                    callback(null, responseBody.toString('utf8'));
-                });
-            } else if(res.statusCode >= 300 && res.statusCode < 400) {
-                if(numRedirects > 0) {
-                    let newURL = new URL(res.headers.location, url);
-                    performFetch(newURL, callback, numRedirects - 1);
                 } else {
-                    callback(new Error('too many redirects'));
+                    callback(new HTTPError(res));
                 }
-            } else {
-                callback(new HTTPError(res));
+            } catch(e) {
+                callback(e);
             }
         });
 
@@ -102,62 +106,70 @@ GET /plugins/hoelzro/first-class-urls/fetch?url=:url
         let matchThisURL = requestURL.searchParams.get('_url') ?? fetchThisURL;
 
         performFetch(fetchThisURL, function(error, html) {
-            if(error != null) {
-                if(error instanceof HTTPError && (error.statusCode == 404 || error.statusCode == 410)) {
-                    response.writeHead(400, 'Page Not Found', {
-                        'Content-Type': 'application/json'
-                    });
-                    response.end('{}');
-                } else {
-                    console.log(error.toString());
-                    response.writeHead(500, 'Internal Server Error', {
-                        'Content-Type': 'application/json'
-                    });
-                    response.end('{}');
-                }
-            } else {
-                let [document] = parseDOM(html);
-
-                let actualDocument;
-
-                while(document != null) {
-                    // XXX just the first one?
-                    if(document.nodeType == 1 && document.tagName == 'html') { // XXX MAGIC NUMBER
-                        actualDocument = document;
-                        break;
+            try {
+                if(error != null) {
+                    if(error instanceof HTTPError && (error.statusCode == 404 || error.statusCode == 410)) {
+                        response.writeHead(400, 'Page Not Found', {
+                            'Content-Type': 'application/json'
+                        });
+                        response.end('{}');
+                    } else {
+                        console.log(error.toString());
+                        response.writeHead(500, 'Internal Server Error', {
+                            'Content-Type': 'application/json'
+                        });
+                        response.end('{}');
                     }
-                    document = document.nextSibling;
-                }
+                } else {
+                    let [document] = parseDOM(html);
 
-                if(!actualDocument) {
-                    response.writeHead(200, 'OK', {
-                        'Content-Type': 'application/json'
+                    let actualDocument;
+
+                    while(document != null) {
+                        // XXX just the first one?
+                        if(document.nodeType == 1 && document.tagName == 'html') { // XXX MAGIC NUMBER
+                            actualDocument = document;
+                            break;
+                        }
+                        document = document.nextSibling;
+                    }
+
+                    if(!actualDocument) {
+                        response.writeHead(200, 'OK', {
+                            'Content-Type': 'application/json'
+                        });
+                        response.end('{}');
+                        return;
+                    }
+
+                    let extractors = [];
+
+                    $tw.modules.forEachModuleOfType('$:/plugin/hoelzro/url-metadata-extractor', (_, module) => extractors.push(module));
+
+                    let extractorPatterns = extractors.map(e => e.pattern);
+
+                    let bestMatch = match(extractorPatterns, matchThisURL);
+                    let bestExtractor = extractors[bestMatch];
+
+                    let result = bestExtractor.extract(fetchThisURL, actualDocument);
+                    if(!(result instanceof Promise)) {
+                        result = Promise.resolve(result);
+                    }
+
+                    result.then(function(metadata) {
+                        response.writeHead(200, 'OK', {
+                            'Content-Type': 'application/json'
+                        });
+                        metadata.url_extractor = bestExtractor.name ?? '';
+                        response.end(JSON.stringify(metadata));
                     });
-                    response.end('{}');
-                    return;
                 }
-
-                let extractors = [];
-
-                $tw.modules.forEachModuleOfType('$:/plugin/hoelzro/url-metadata-extractor', (_, module) => extractors.push(module));
-
-                let extractorPatterns = extractors.map(e => e.pattern);
-
-                let bestMatch = match(extractorPatterns, matchThisURL);
-                let bestExtractor = extractors[bestMatch];
-
-                let result = bestExtractor.extract(fetchThisURL, actualDocument);
-                if(!(result instanceof Promise)) {
-                    result = Promise.resolve(result);
-                }
-
-                result.then(function(metadata) {
-                    response.writeHead(200, 'OK', {
-                        'Content-Type': 'application/json'
-                    });
-                    metadata.url_extractor = bestExtractor.name ?? '';
-                    response.end(JSON.stringify(metadata));
+            } catch(e) {
+                console.error(e);
+                response.writeHead(500, 'Internal Server Error', {
+                    'Content-Type': 'application/json'
                 });
+                response.end('{}');
             }
         });
     };
