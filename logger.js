32590af89ac81208176c3c892f5085a93623b6ec
Rob Hoelz <rob@hoelz.ro>
2021-09-14 07:21:43 -0500

    Add basic logger library
    …that just wraps TiddlyWiki's logger, so that I have a centralized
    place to manage lazy initialization and the save buffer, and so that
    can add my own methods

---
+++ logger.js
@@ -0,0 +1,27 @@
+/*\
+module-type: library
+
+\*/
+
+(function() {
+    let logger = {};
+
+    let innerLogger;
+
+    logger.log = function(...args) {
+        if(!innerLogger) {
+            if(!$tw.utils.Logger) {
+                // XXX just drop the logs on the floor?
+                return;
+            }
+            innerLogger = new $tw.utils.Logger('first-class-urls', {
+                saveLimit: 1024 * 1024,
+            });
+        }
+        return innerLogger.log(...args);
+    };
+
+    logger.error = logger.log; // XXX for now
+
+    module.exports = logger;
+})();
