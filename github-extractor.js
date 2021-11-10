ccc3604c1b8f8669cd827bbb651ebf1dd7fcb3b3
Rob Hoelz <rob@hoelz.ro>
2021-11-10 07:50:34 -0600

    Appease eslint

--- github-extractor.js
+++ github-extractor.js
@@ -19,7 +19,7 @@ module-type: $:/plugin/hoelzro/url-metadata-extractor
                 metadata.description = meta.attribs.content.trim();
                 metadata.description = metadata.description.replace(/[.]\s*Contribute to \w+\/\w+ development by creating an account on GitHub[.]\s*$/, '');
             } else if(meta.attribs.name == 'twitter:title') {
-                [ , metadata.github_author, metadata.github_project] = meta.attribs.content.trim().match(new RegExp('(\\w+)/(\\S+):\\s'));
+                [, metadata.github_author, metadata.github_project] = meta.attribs.content.trim().match(new RegExp('(\\w+)/(\\S+):\\s'));
             }
         }
 
