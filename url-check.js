d76886be10bc9e91308f499d4c65b07319ccd639
Rob Hoelz <rob@hoelz.ro>
2023-05-14 13:07:46 -0500

    Change passing of location url-check filter
    On the TiddlyWiki 5.3 prerelease, the core filter code calls
    `evaluateVariable` rather than `getVariable`, which our faux widget
    doesn't implement.  The whole "pass a fake widget with a minimal set of
    methods" thing always felt kinda flimsy to me, but I didn't know of a
    better way to pass a string from JavaScript into the filter system.
    
    However, now I do of one - I can just pass the location I'm looking for
    in as the sole input for the filter!

--- url-check.js
+++ url-check.js
@@ -5,19 +5,10 @@ module-type: library
 
 // XXX location is expected to be a canonicalized URL
 module.exports = function(location) {
-    let fauxWidget = {
-        getVariable(variableName, options) {
-            if(variableName == 'location') {
-                return location;
-            }
-            if(options && 'defaultValue' in options) {
-                return options.defaultValue;
-            } else {
-                return null;
-            }
-        }
+    let source = function(iter) {
+      iter(null, location);
     };
+    let [tiddler] = $tw.wiki.filterTiddlers('[listed[location]has[url_tiddler]!has[draft.of]]', null, source);
 
-    let [tiddler] = $tw.wiki.filterTiddlers('[field:location<location>has[url_tiddler]!has[draft.of]]', fauxWidget);
     return tiddler;
 };
