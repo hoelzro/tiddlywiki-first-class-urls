362c771b693191d874a0aff81938ec83136271ce
Rob Hoelz <rob@hoelz.ro>
2023-06-04 16:04:54 -0500

    Fix up url-check change to comply with eslint rules

--- url-check.js
+++ url-check.js
@@ -5,9 +5,10 @@ module-type: library
 
 // XXX location is expected to be a canonicalized URL
 module.exports = function(location) {
-    let source = function(iter) {
+    function source(iter) {
       iter(null, location);
-    };
+    }
+
     let [tiddler] = $tw.wiki.filterTiddlers('[listed[location]has[url_tiddler]!has[draft.of]]', null, source);
 
     return tiddler;
